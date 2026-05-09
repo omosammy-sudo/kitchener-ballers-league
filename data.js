1397f5713ddd324f2779616fc3850eae96f9717b
omosammy-sudo <omosammy@gmail.com>
2026-05-09 13:41:33 -0400

    Update data.js

--- data.js
+++ data.js
@@ -1,9 +1,9 @@
 // UPDATE WEEKLY HERE
 
 const fixtures = [
-  { pitch: "Pitch 1", match: "City Lion FC vs The Bull FC", referee: "Bello" },
-  { pitch: "Pitch 2", match: "Mighty Elephants FC vs Junior Eagles FC", referee: "Kingsley" },
-  { pitch: "Pitch 3", match: "Mad Ballers FC vs Onyx Team", referee: "Goody" }
+  { pitch: "Pitch 1", match: "City Lion FC vs Junior Eagles FC", referee: "Goody" },
+  { pitch: "Pitch 2", match: "The Bull FC vs Onyx Team", referee: "Kingsley" },
+  { pitch: "Pitch 3", match: "Mighty Elephants FC vs Mad Ballers FC", referee: "Bello" }
 ];
 
 const results = [
@@ -22,50 +22,72 @@ const results = [
       { match: "Mad Ballers FC vs The Bull FC", score: "3 - 3" },
       { match: "Onyx FC vs Junior Eagles FC", score: "9 - 4" }
     ]
+  },
+  {
+    matchday: 3,
+    games: [
+      { match: "Mad Ballers FC vs Onyx Team", score: "4 - 9" },
+      { match: "City Lion FC vs The Bull FC", score: "11 - 2" },
+      { match: "Mighty Elephants FC vs Junior Eagles FC", score: "18 - 2" }
+    ]
   }
 ];
+
 const standings = [
-  { team: "The Bull FC", p: 2, w: 1, d: 1, l: 0, gf: 9, ga: 6, gd: "+3", pts: 4 },
-  { team: "City Lion FC", p: 2, w: 1, d: 1, l: 0, gf: 7, ga: 5, gd: "+2", pts: 4 },
-  { team: "Mighty Elephants FC", p: 2, w: 1, d: 1, l: 0, gf: 10, ga: 9, gd: "+1", pts: 4 },
-  { team: "Onyx Team", p: 2, w: 1, d: 0, l: 1, gf: 14, ga: 10, gd: "+4", pts: 3 },
-  { team: "Mad Ballers FC", p: 2, w: 0, d: 1, l: 1, gf: 4, ga: 6, gd: "-2", pts: 1 },
-  { team: "Junior Eagles FC", p: 2, w: 0, d: 0, l: 2, gf: 7, ga: 15, gd: "-8", pts: 0 }
+  { team: "Mighty Elephants FC", p: 3, w: 2, d: 1, l: 0, gf: 28, ga: 11, gd: "+17", pts: 7 },
+  { team: "City Lion FC", p: 3, w: 2, d: 1, l: 0, gf: 18, ga: 7, gd: "+11", pts: 7 },
+  { team: "Onyx Team", p: 3, w: 2, d: 0, l: 1, gf: 23, ga: 14, gd: "+9", pts: 6 },
+  { team: "The Bull FC", p: 3, w: 1, d: 1, l: 1, gf: 11, ga: 17, gd: "-6", pts: 4 },
+  { team: "Mad Ballers FC", p: 3, w: 0, d: 1, l: 2, gf: 8, ga: 15, gd: "-7", pts: 1 },
+  { team: "Junior Eagles FC", p: 3, w: 0, d: 0, l: 3, gf: 9, ga: 33, gd: "-24", pts: 0 }
 ];
 
 const scorers = [
-  { player: "Leke Kitchener", team: "Onyx Team", goals: 4 },
+  { player: "Leke Kitchener", team: "Onyx Team", goals: 8 },
+  { player: "Fuhad", team: "Mighty Elephants FC", goals: 8 },
+  { player: "Mofi", team: "Mighty Elephants FC", goals: 5 },
+
+  { player: "Trap Money", team: "Mad Ballers FC", goals: 3 },
   { player: "Michael", team: "Onyx Team", goals: 3 },
+  { player: "Matosa", team: "The Bull FC", goals: 3 },
+  { player: "Drogba", team: "City Lion FC", goals: 3 },
+
   { player: "Daniel Mofe", team: "The Bull FC", goals: 2 },
   { player: "Temidayo", team: "City Lion FC", goals: 2 },
   { player: "Thomas", team: "Onyx Team", goals: 2 },
-  { player: "Ifeanyi", team: "City Lion FC", goals: 2 },
-  { player: "Matosa", team: "The Bull FC", goals: 3 },
+  { player: "Ifeanyi", team: "City Lion FC", goals: 3 },
   { player: "Larry", team: "Junior Eagles FC", goals: 2 },
-
   { player: "Ovie", team: "Junior Eagles FC", goals: 2 },
   { player: "Short Abdul", team: "The Bull FC", goals: 2 },
-  { player: "Fuhad", team: "Mighty Elephants FC", goals: 2 },
   { player: "Layi", team: "Mighty Elephants FC", goals: 2 },
-  { player: "Hooda", team: "Mighty Elephants FC", goals: 2 },
+  { player: "Hooda", team: "Mighty Elephants FC", goals: 4 },
   { player: "Indomie", team: "Mighty Elephants FC", goals: 2 },
+  { player: "Josh", team: "Onyx Team", goals: 2 },
+  { player: "Ola", team: "Onyx Team", goals: 3 },
+  { player: "Justice", team: "City Lion FC", goals: 2 },
+  { player: "Ayo", team: "City Lion FC", goals: 2 },
+  { player: "Ikenna", team: "Mighty Elephants FC", goals: 2 },
+  { player: "Sammy", team: "Mighty Elephants FC", goals: 2 },
+  { player: "Wisdom", team: "Mighty Elephants FC", goals: 2 },
 
   { player: "Eromosele", team: "Junior Eagles FC", goals: 1 },
   { player: "Isaac", team: "Junior Eagles FC", goals: 1 },
   { player: "Johnny", team: "The Bull FC", goals: 1 },
-  { player: "OG", team: "The Bull FC", goals: 1 },
+  { player: "OG", team: "The Bull FC", goals: 2 },
   { player: "Felix", team: "City Lion FC", goals: 1 },
-  { player: "Trap Money", team: "Mad Ballers FC", goals: 1 },
   { player: "Slim Manny", team: "Onyx Team", goals: 2 },
   { player: "Olamide", team: "Onyx Team", goals: 1 },
-  { player: "Mofi", team: "Mighty Elephants FC", goals: 1 },
   { player: "Kento", team: "Mighty Elephants FC", goals: 1 },
   { player: "Megai", team: "Mad Ballers FC", goals: 1 },
   { player: "Olu", team: "Mad Ballers FC", goals: 1 },
-  { player: "Japhet", team: "City Lion FC", goals: 1 },
-  { player: "Leke Waterloo", team: "City Lion FC", goals: 1 },
+  { player: "Japhet", team: "City Lion FC", goals: 2 },
+  { player: "Leke Waterloo", team: "City Lion FC", goals: 2 },
   { player: "Ayokay", team: "Onyx Team", goals: 1 },
-  { player: "Henry", team: "Junior Eagles FC", goals: 1 }
+  { player: "Henry", team: "Junior Eagles FC", goals: 1 },
+  { player: "Neme", team: "Mad Ballers FC", goals: 1 },
+  { player: "Wilson", team: "Mad Ballers FC", goals: 1 },
+  { player: "Ziko", team: "The Bull FC", goals: 1 },
+  { player: "Big Manny", team: "Onyx Team", goals: 1 }
 ];
 
 const ownGoals = [
